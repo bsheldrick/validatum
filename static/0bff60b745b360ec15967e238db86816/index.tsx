1f0097b73d28eaf5861213a25025a8a254f4036b
Brad Sheldrick <brad@bnsweb.com.au>
2020-05-21 15:07:48 +1000

    Updates

---
+++ static/0bff60b745b360ec15967e238db86816/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react";
+import Layout from "../components/layout";
+
+function Index(): ReactElement {
+  return (
+    <Layout>
+      <h1>Validatum</h1>
+    </Layout>
+  );
+}
+
+export default Index;
